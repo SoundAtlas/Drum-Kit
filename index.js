ff99c9f6c84692e6bad482511415f2ea8096d2fe
Andreas Hemmer <andreas.hemmer@hotmail.com>
2025-05-25 17:49:35 +0200

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+//preloading sounds
+const sounds = {
+  a: new Audio("sounds/Kick.wav"),
+  s: new Audio("sounds/snare.mp3"),
+  d: new Audio("sounds/crash.mp3"),
+  h: new Audio("sounds/tom-1.mp3"),
+  j: new Audio("sounds/tom-2.mp3"),
+  k: new Audio("sounds/tom-3.mp3"),
+  l: new Audio("sounds/tom-4.mp3"),
+};
+
 //Detecting button press
 
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
@@ -5,6 +16,8 @@ for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
     var buttonInnerHTML = this.innerHTML;
 
     makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
@@ -12,6 +25,8 @@ for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
 document.addEventListener("keydown", function (event) {
   makeSound(event.key);
+
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -52,7 +67,17 @@ function makeSound(key) {
       break;
 
     default:
-      console.log(buttonInnerHTML);
+      console.log(key);
       break;
   }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
